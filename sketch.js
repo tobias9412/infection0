cb5c31ed7d3605bf417d4ed13cb518cfad51f2c8
tobias9412 <52341036+tobias9412@users.noreply.github.com>
2020-04-04 22:02:07 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,13 +3,20 @@ let rowCount;
 let display = 1;
 let mindist = 75;
 
-let n = [];
-let id = [];
-let journal = [];
-let date = [];
-let connection = [];
+let n              = [];
+let id             = [];
+let journal        = [];
+let date           = [];
+let connection     = [];
 let classification = [];
-let connected = [];
+let connected      = [];
+let endJournal     = [];
+let endStatus      = [];
+
+let strConfirmed  = 0;
+let strDischarged = 0;
+let strDeceased   = 0;
+
 
 function preload() {
   table = loadTable('https://raw.githubusercontent.com/tobias9412/infection0/master/data.csv',  'csv', 'header');
@@ -26,6 +33,7 @@ function setup() {
     id[r] = r + 1;
     journal[r] = table.get(r, 'Journal');
     date[r] = table.get(r, 'Report date');
+
     connection[r] = table.get(r, 'Connection');
     connected[r] = [];
     connected[r] = int(split(connection[r], '/'));
@@ -51,8 +59,24 @@ function setup() {
         break;
     }
 
-    n[r] = new Node(id[r], journal[r], connected[r], classification[r], windowWidth / 2 - mindist/2 + random(mindist), windowHeight / 2 - mindist/2 + random(mindist));
-    console.log(n[r].id, n[r].journal, n[r].connected, n[r].classification, n[r].pos); 
+    endJournal[r] = table.get(r, 'End journal');
+    switch (table.get(r, 'End status')) {
+      case "Discharged":
+        endStatus[r] = 1;
+        break;
+      case "Deceased":
+        endStatus[r] = 2;
+        break;
+      default:
+        endStatus[r] = 0;
+        break;
+      }
+
+
+
+
+n[r] = new Node(id[r], journal[r], connected[r], classification[r], endJournal[r], endStatus[r], windowWidth / 2 - mindist/2 + random(mindist), windowHeight / 2 - mindist/2 + random(mindist));
+    console.log(n[r].id, n[r].journal, n[r].connected, n[r].classification, n[r].endJournal, n[r].endStatus); 
   }
 }
 
@@ -94,48 +118,79 @@ function draw() {
 
   for (let r = 0; r < rowCount; r++) {
     //node display
-    if (n[r].journal <= display) {(HSB, 255);
+    if (n[r].journal <= display) {
+      // hospitalised
+      if (n[r].endJournal > display || n[r].endStatus == 0) {
       fill(127+n[r].intensity*30, 149, 0);
       noStroke();
-      ellipse(n[r].pos.x, n[r].pos.y, 10+n[r].intensity/2, 10+n[r].intensity/2);
-      fill(255);
-      textSize(15);
-      textAlign(CENTER, CENTER);
-      //text(n[r].id, n[r].pos.x, n[r].pos.y);
+    } else { 
+    if (n[r].endJournal <= display && n[r].endStatus == 1) {
+      stroke(127+n[r].intensity*30, 149, 0);
+      fill(200);}
+    if (n[r].endJournal <= display && n[r].endStatus == 2) {
+      noStroke();
+      fill(0);
+      }
     }
 
-    //info display
-    if (n[r].journal == display) {
-      fill(200);
-     rect(0, screenH-30,  windowWidth, 50);
-     fill(100);
-      noStroke();
-      textSize(20);
-      textAlign(LEFT, TOP);
-      text("date:"+date[r], 20, screenH-30);
-       textAlign(RIGHT, TOP);
-     text("confirmed:" + id[r], screenW-20, screenH-30);
-    }
-        n[r].intensity = 0;
+    ellipse(n[r].pos.x, n[r].pos.y, 10+n[r].intensity/2, 10+n[r].intensity/2);
+  }
+}
+  //info display
+
+  fill(50);
+  noStroke();
+  textSize(20);
+  textAlign(LEFT, BOTTOM);
+  if (display < 10)
+    text("Date: " + (display+22) + "/1/2020" , 20, screenH-85);
+  if (display >= 10 && display < 39)
+    text("Date: " + (display-9) + "/2/2020" , 20, screenH-85);
+  if (display >= 39)
+    text("Date: " + (display-38) + "/3/2020" , 20, screenH-85);
+
+  textAlign(LEFT, BOTTOM);
+  text("Discharged Cases: " + strDischarged + "\nDeceased Cases: " + strDeceased + "\nConfirmed / Probable Cases: " + strConfirmed, 20, screenH-10);
+
+
+   for (let r = 0; r < rowCount; r++) 
+       n[r].intensity = 0;
+  
+}
+
+
+/*function mouseClicked() {
+  if (display < n[rowCount-1].journal)
+    display += 1;
+}*/
+
+function touchEnded() {
+  strDischarged = 0;
+  strDeceased = 0;
+
+  if (display < n[rowCount-1].journal)
+    display ++;
+
+  for (let r = 0; r < rowCount; r++) {
+
+    if (n[r].journal <= display)
+      strConfirmed = id[r];
+
+    if (n[r].endJournal <= display && n[r].endStatus == 1)
+      strDischarged ++;
+        if (n[r].endJournal <= display && n[r].endStatus == 2) 
+      strDeceased   ++;
   }
 }
 
-function mouseClicked() {
-  if (display < n[rowCount-1].journal)
-    display++;
-}
-
-function touchEnded() {
-  if (display < n[rowCount-1].journal)
-    display++;
-}
-
 class Node {
-  constructor(i, j, c, cl, x, y) {
+  constructor(i, j, c, cl, ej, es, x, y) {
     this.id = i;
     this.journal = j;
     this.connected = c;
     this.classification = cl;
+    this.endJournal = ej;
+    this.endStatus = es;
     this.pos = createVector(x, y);
     this.intensity = 0;
   }
